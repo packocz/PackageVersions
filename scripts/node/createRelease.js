f6202bdee3d63e45bd761ef67053ed2775925b4b
packocz <34923415+packocz@users.noreply.github.com>
2024-01-17 14:17:08 +0100

    md

--- scripts/node/createRelease.js
+++ scripts/node/createRelease.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+const changeLogMd = fs.readFileSync('changelog/test.md', { encoding: 'utf8' });
+
 const repo = {
 	owner: 'packocz',
 	name: 'PackageVersions'
@@ -8,7 +11,7 @@ const release = {
 	tag: 'v1.0.0',
 	commitsh: 'create-release',
 	name: 'v1.0.0',
-	body: 'Initial release',
+	body: changeLogMd,
 	isDraft: false
 };
 const github = require('./github.js');
